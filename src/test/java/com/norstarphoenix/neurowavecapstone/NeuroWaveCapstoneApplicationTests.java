fa57d12d4c4251d123a01b93bd4f2f97e740742d
Norstar Phoenix <norstar1983@gmail.com>
2024-12-01 03:18:23 -0500

    setting up repo

---
+++ src/test/java/com/norstarphoenix/neurowavecapstone/NeuroWaveCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.norstarphoenix.neurowavecapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroWaveCapstoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
