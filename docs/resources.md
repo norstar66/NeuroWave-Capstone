c474de5f9bfe5020d8788f08d1e620ef3e27f172
Norstar Phoenix <norstar1983@gmail.com>
2024-12-01 05:12:17 -0500

    mvn clean install -DskipTests

---
+++ docs/resources.md
@@ -0,0 +1,4 @@
+# Resources
+
+- Spring Boot Documentation: [https://spring.io/projects/spring-boot](https://spring.io/projects/spring-boot)
+- Thymeleaf Documentation: [https://www.thymeleaf.org/](https://www.thymeleaf.org/)
--- src/test/java/com/norstarphoenix/neurowavecapstone/NeuroWaveCapstoneApplicationTests.java
+++ src/test/java/com/norstarphoenix/neurowavecapstone/NeuroWaveCapstoneApplicationTests.java
@@ -1,8 +1,10 @@
 package com.norstarphoenix.neurowavecapstone;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class NeuroWaveCapstoneApplicationTests {
 
